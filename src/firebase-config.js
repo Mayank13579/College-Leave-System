c51798b5eb14dc15085ba334467b004127d817c9
Mayank13579 <mayankupadhyay13579@gmail.com>
2024-01-15 17:20:12 +0530

    firebase updates

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -3,14 +3,14 @@ import {getFirestore} from "@firebase/firestore";
 import {getAuth} from "firebase/auth";
 import {getStorage} from "firebase/storage";
 const firebaseConfig = {
-    apiKey: "AIzaSyDAxJjBoY1pGB9VJvrljbv6sZ4Gsar0xIM",
-    authDomain: "leavemanagement-718f8.firebaseapp.com",
-    projectId: "leavemanagement-718f8",
-    storageBucket: "leavemanagement-718f8.appspot.com",
-    messagingSenderId: "215370695977",
-    appId: "1:215370695977:web:eebd7ebf19d4562245a338",
-    measurementId: "G-1J9H0FCF32"
-  };
+  apiKey: "AIzaSyAYxF1pa2J5Qu1iDyyiuGk25xaA0F8U3-I",
+  authDomain: "test-e8f8b.firebaseapp.com",
+  projectId: "test-e8f8b",
+  storageBucket: "test-e8f8b.appspot.com",
+  messagingSenderId: "788199825618",
+  appId: "1:788199825618:web:51676ee4a4fc4f79c60d0b",
+  measurementId: "G-WP50PE26E5"
+};
   const fire=()=>{
     
   }
